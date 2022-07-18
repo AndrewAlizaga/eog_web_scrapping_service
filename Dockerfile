b5f6ea9ffc7a32718b77b16f3739eba4cfceb622
Andrew Alizaga <aalizaga@vozy.co>
2022-07-17 20:04:56 -0600

    addres

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,21 @@ COPY package*.json ./
 #full fill dependencies
 RUN npm install 
 
+RUN apt-get update && apt-get upgrade 
+#RUN apt-get resdis-server
+# --update add redis 
+
+#install -y reids-server
+
+#RUN apt-get install redis-server
+
 RUN npm ci --only=production
 
 COPY . . 
 
+ENV EOG_WEB_SCRAPPER_ADDR_PORT=127.0.0.1:50051
+ENV REDIS_PORT=6380
+ENV REDIS_HOST=127.0.0.1
 EXPOSE 50051
 
 CMD ["node", "server.js"]
--- services/db/redis/index.js
+++ services/db/redis/index.js
@@ -2,17 +2,20 @@ const redis = require("redis");
 
  const client = redis.createClient({
     host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT || 6379),
+    port: parseInt(6380),
   });
 
 console.log('redis defined')
 
 
 client.on('connect', function() {
+  console.log(process.env.REDIS_PORT)
   console.log('Redis Connected!');
 });
 
 client.on("error", (err) => {
+  console.log('there was an error')
+  console.log(process.env.REDIS_PORT)
   console.log(err);
 });
 
