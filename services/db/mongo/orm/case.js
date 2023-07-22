a7a241edb7ee76b9932070710a765239ca6f1216
AndrewAlizaga <andrewatld@gmail.com>
2023-07-22 00:39:49 -0600

    added entities orms

---
+++ services/db/mongo/orm/case.js
@@ -0,0 +1,19 @@
+let mongoClient = require("../mongo")
+
+
+async function SaveCase(searchCase){}
+
+async function SaveCases(searchCases){}
+
+async function DeleteCaseByID(searchCaseID){}
+
+async function GetCaseByID(){caseID}
+
+
+
+module.exports = {
+    SaveCase, 
+    SaveCases, 
+    DeleteCaseByID, 
+    GetCaseByID,
+}
