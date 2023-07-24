d8b10cc0cfff021a87e4e516feddcc4115588ee1
AndrewAlizaga <andrewatld@gmail.com>
2023-07-24 13:36:27 -0600

    suppong or Lead entityon db

--- services/db/mongo/orm/lead.js
+++ services/db/mongo/orm/lead.js
@@ -1,24 +1,52 @@
 let mongoClient = require("../mongo")
+let EntityORM = require("./entity")
+
+let tableName = "leads"
+let primaryKey = "id"
+let groupKey = "owner"
+    
+
+    async function SaveLead(searchLead){
+        let leadResult = await EntityORM.SaveEntity(searchLead, tableName)
+        console.log(leadResult)
+        return leadResult
+    }
+
+    async function UpdateLead(searchLead){
+        let leadResult = await EntityORM.UpdateEntity(searchLead, searchLead.id, tableName)
+        console.log(leadResult)
+        return leadResult
+    }
+
+    async function SaveLeads(searchLeads){
+
+    }
+    
+    async function DeleteLeadByID(id){
+        let leadResult = await EntityORM.DeleteEntityByID(id, tableName)
+        console.log(leadResult)
+        return leadResult
+    }
+    
+    async function GetLeadByID(id){
+        let leadResult = await EntityORM.GetEntityByID(id, tableName)
+        console.log(leadResult)
+        return leadResult
+    }
 
 
-async function SaveLead(lead){}
-
-async function SaveLeads(leads){}
-
-async function DeleteLeadsByCaseID(caseID){}
-
-async function DeleteLeadByID(leadID){}
-
-async function GetLeadByID(){leadID}
-
-async function GetLeadsByCaseID(caseID){}
+    async function GetLeadsByID(id){
+        let leadResult = await EntityORM.GetEntitiesByID(id, groupKey, tableName)
+        console.log(leadResult)
+        return leadResult
+    }
 
 
 module.exports = {
-    SaveLead, 
-    SaveLeads, 
-    DeleteLeadByID, 
-    DeleteLeadsByCaseID,
-    GetLeadByID,
-    GetLeadsByCaseID
+SaveLead,
+UpdateLead,
+SaveLeads,
+DeleteLeadByID,
+GetLeadByID,
+GetLeadsByID
 }
